100e4e118efd84dd5a521e930de4f16113aa7431
T_Tanya <tikhonovskyat@gmail.com>
2020-12-08 16:06:22 +0300

    2 commit

--- server3.js
+++ server3.js
@@ -1,14 +1,19 @@
 import { graphql, buildSchema } from 'graphql';
 import express from 'express';
 const { graphqlHTTP } = require('express-graphql');;
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 let schema = buildSchema(`
  type Query {
- hello: String
+ number: [Int]
  }
 `);
 let root = {
- hello: () => {
-    return 'Hello 3!';
+ number: () => {
+    return [ 2, 5, 7, 8, 9, getRandomInt(2)];
  },
 };
 let app = express();
